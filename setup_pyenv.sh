11b7fcc5b2c8877c29dfb5383948894dc598cd60
Kilian Pfeiffer <kilian-yutaka.pfeiffer@xarvio.com>
2020-08-31 10:38:44 +0200

    Add more setup scripts

---
+++ setup_pyenv.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+
---
+++ setup_software.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+
+# clipboard
+sudo apt-get install xclip
+
+# docker
+sudo apt install docker.io
+sudo systemctl unmask docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+#
+sudo apt install sshfs
